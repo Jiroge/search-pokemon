7adf37c0f5624d56168050063c47301827ebb801
Jiroge <s.jiroge@gmail.com>
2024-12-28 22:58:55 +0700

    prepare for deploy on vercel

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,7 +1,7 @@
 import { forwardRef } from "react";
 
 export const Card = forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
-  ({ className, ...props }, ref) => (
+  ({ ...props }, ref) => (
     <div
       ref={ref}
       className="rounded-lg border border-gray-200 bg-white shadow-sm dark:border-gray-800 dark:bg-gray-950"
@@ -12,7 +12,7 @@ export const Card = forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivEleme
 Card.displayName = "Card";
 
 export const CardHeader = forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
-  ({ className, ...props }, ref) => (
+  ({ ...props }, ref) => (
     <div
       ref={ref}
       className="flex flex-col space-y-1.5 p-6"
@@ -23,7 +23,7 @@ export const CardHeader = forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDi
 CardHeader.displayName = "CardHeader";
 
 export const CardContent = forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
-  ({ className, ...props }, ref) => (
+  ({ ...props }, ref) => (
     <div ref={ref} className="p-6 pt-0" {...props} />
   )
 );
